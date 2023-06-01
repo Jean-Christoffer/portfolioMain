9cec585a46a4a1e2f12af0080d96716805ed27c3
half6 <christofferhalf6@gmail.com>
2023-06-01 11:27:35 +0200

    cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,24 +1,66 @@
 /* eslint-disable react/prop-types */
 import { Paper,Box,Typography } from "@mui/material"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LanguageIcon from '@mui/icons-material/Language';
 export default function Cards(props){
     const { details } = props
+
+    const [active,setActive] = useState(false)
+
+    function handleActive(){
+        setActive(prevState => !prevState)
+    }
+
     return(
         <>
-        <Paper 
-        sx={{ 
-            mb:1,
-         }}
+        <Paper
+        sx={{
+          maxWidth: "400px",
+          height: "100%",
+          transition: "height 0.3s",
+          overflow:'hidden'
+        }}
+
+      >
+        <Box
+          onClick={handleActive}
+          sx={{
+            maxWidth: "100%",
+            transition: "max-width 0.3s",
+            cursor:'pointer',
+          
+
+          }}
+         
         >
-            <Box
-            sx={{ 
-                maxWidth:'300px'
-             }}
-            > 
-                <img src={details.img}/>
-            </Box>
-                
-            <Typography  component='p' variant="body">{details.title}</Typography>
-        </Paper>
+          <img src={details.img} alt={details.title} />
+          <Typography component="h1" variant="h5"
+          sx={{ 
+            display:'block',
+            background:'black',
+            color:'white',
+            p:1
+           }}
+          >
+          {details.title}
+        </Typography>
+        </Box>
+
+
+          {active && 
+          <Box p={1}>
+             <Typography component="p" variant="body">{details.excerpt}</Typography>
+             <Link to={details.git} target="blank"><GitHubIcon/></Link>
+             <Link to={details.web} target="blank"><LanguageIcon/></Link>
+             <Typography component="p" variant="body">{details.type}</Typography>
+             <Box sx={{ display:'flex',gap:1,alignItems:'center',height:'50px' }}>
+             {details.tools.map((logo,i) => <Box key={i} sx={{ maxWidth:'30px' }}><img   src={logo}/></Box>)}
+             </Box>
+          </Box>
+          }
+      </Paper>
         </>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,39 +1,82 @@
 import Cards from "./Cards"
-import {Container,Box,Typography} from '@mui/material'
+import {Container,Box} from '@mui/material'
 
 export default function Projects(){
     const projectList =
  [
     {
      title:'Wave',
-     excerpt:'',
-     type:'',
+     excerpt:'This is my year one exam. to put together what we',
+     type:'Project exam',
      img:'images/wave1.jpg',
-     id:1
+     id:1,
+     git:'https://github.com/Noroff-FEU-Assignments/project-exam-1-Jean-Christoffer',
+     web:'https://charming-mousse-954d3d.netlify.app/',
+     tools:['images/css3.svg','images/html5.svg','images/wordpress.svg','images/javascript.svg']
     },
 
     {
         title:'Museum',
-        excerpt:'',
-        type:'',
+        excerpt:'whatever',
+        type:'Project exam',
         img:'images/museum.jpg',
-        id:2
+        id:2,
+        git:'https://github.com/Jean-Christoffer/Museum',
+        web:'https://spectacular-sorbet-d98fe8.netlify.app/index.html',
+        tools:['images/css3.svg','images/html5.svg','images/javascript.svg']
     },
+
 
     {
         title:'Gamehub',
-        excerpt:'',
-        type:'',
+        excerpt:'asdasd',
+        type:'Cross course assigment',
         img:'images/gamehub.jpg',
-        id:3
+        id:3,
+        git:'https://github.com/Jean-Christoffer/gamehubFinal',
+        web:'https://gamehub-f-inal.vercel.app/',
+        tools:['images/css3.svg','images/html5.svg','images/wordpress.svg','images/javascript.svg','images/three.svg']
+    },
+    {
+        title:'Portfolio',
+        excerpt:'asdasd',
+        type:'Course assigment',
+        img:'images/port.jpg',
+        id:3,
+        git:'https://github.com/Jean-Christoffer/portfolioMain',
+        web:'https://portfolio-main-mauve.vercel.app/',
+        tools:['images/react.svg','images/mui.svg','images/three.svg']
+    },
+    {
+        title:'MovieBase',
+        excerpt:'asdasd',
+        type:'Personal',
+        img:'images/movie.jpg',
+        id:3,
+        git:'https://github.com/Jean-Christoffer/Movie-base',
+        web:'https://movie-base-mauve.vercel.app/',
+        tools:['images/react.svg','images/mui.svg']
     },
 ]
 
     return(
         <>
-            <Container>
-                <Typography component='p' variant="body">Projects</Typography>
-                <Box>
+            <Container
+            sx={{ 
+                display:'flex',
+                flexDirection:'column',
+
+             }}
+             >
+                <Box sx={{ 
+                    display:'flex', 
+                    gap:2,
+                    flexWrap:'wrap',
+                    justifyContent:'center',
+                    mt:5
+                    
+           
+                     }}>
                     {projectList.map(project=> <Cards key={project.id} details={project} />)}
                 </Box>
             </Container>
